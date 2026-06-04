12e00eeb3c0e355870985058dced971689d0b82a
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-06-04 12:00:00 +0000

    Add nested-svm tests: vmload-negative, vmsave-negative and vmrun
    Add nested SVM test cases for VMLOAD, VMSAVE and VMRUN using a shared L2.
    
    The negative tests assert the error handling of using a test matrix
    of execution of VMLOAD and VMSAVE in a matrix of contexts:
    - User and Kernel context
    - EFER.SVME enabled and disabled
    - EAX VMCB register aligned, unaligned, or outside of physical range
    - CPL0 and CPL3
    
    The tests create and use shared headers and modules for L2 setup.
    
    Also create a new category nested-svm for running the category using:
    ./xtf-runner nested-svm
    
    Based on an initial experiment by Ross to run a minimal L2.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@citrix.com>

--- build/common.mk
+++ build/common.mk
@@ -1,4 +1,4 @@
-ALL_CATEGORIES     := special functional xsa utility in-development
+ALL_CATEGORIES     := special functional xsa utility in-development nested-svm
 
 ALL_ENVIRONMENTS   := pv64 pv32pae hvm64 hvm32pae hvm32pse hvm32
 
@@ -6,6 +6,7 @@ PV_ENVIRONMENTS    := $(filter pv%,$(ALL_ENVIRONMENTS))
 HVM_ENVIRONMENTS   := $(filter hvm%,$(ALL_ENVIRONMENTS))
 32BIT_ENVIRONMENTS := $(filter pv32% hvm32%,$(ALL_ENVIRONMENTS))
 64BIT_ENVIRONMENTS := $(filter pv64% hvm64%,$(ALL_ENVIRONMENTS))
+SVM_ENVIRONMENTS   := hvm64
 
 # $(env)_guest => pv or hvm mapping
 $(foreach env,$(PV_ENVIRONMENTS),$(eval $(env)_guest := pv))
